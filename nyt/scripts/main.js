398bda83566f204345036d4b8d3d3ba84971a762
Michael Nishiguchi <michaelnishiguchi@Michaels-MacBook-Pro.local>
2020-07-24 17:11:03 -0700

    filter/search done

--- nyt/scripts/main.js
+++ nyt/scripts/main.js
@@ -1,4 +1,5 @@
 apiKey = 'p7qxACKrkRzaWoswy2mvAGBGQ0faP7oW';
+// test url https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=p7qxACKrkRzaWoswy2mvAGBGQ0faP7oW
 
 let weatherRequest = new XMLHttpRequest();
 let urlAPI = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=p7qxACKrkRzaWoswy2mvAGBGQ0faP7oW';
@@ -28,25 +29,35 @@ function getTopStories() {
 
 function appendData(data) {
 	let parent = document.getElementById('topStories');
+
 	for (var i = 0; i < data.results.length; i++) {
 		var article = document.createElement('article');
 		article.className = 'story';
-		article.innerHTML =
-			'<img src="' +
-			data.results[i].media[0]['media-metadata'][2]['url'] +
-			'"><h2>' +
-			data.results[i].title +
-			'</h2><ul><li>' +
-			data.results[i].abstract +
-			'</li></ul>';
+
+		var img = document.createElement('img');
+		var h2 = document.createElement('h2');
+		var p = document.createElement('p');
+
+		img.src = data.results[i].media[0]['media-metadata'][2]['url'];
+		h2.innerHTML = '<a href="' + data.results[i]['url'] + '">' + data.results[i].title;
+		p.innerHTML = data.results[i].abstract;
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(p);
+
 		document.getElementById('topStories').appendChild(article);
 	}
 }
 
 function customFilter() {
-	var wordList = [];
+	//clear past results
+	clearBox();
 
+	// create array with filter terms with upper and lower case
+	var wordList = [];
 	for (var i = 1; i < 6; i++) {
+		// max of 5 filter terms
 		var id = 'filter' + i;
 
 		var filterValue = document.getElementById(id).value;
@@ -57,31 +68,101 @@ function customFilter() {
 		}
 	}
 
-	let urlApi = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=' + apiKey;
-
-	let testUrl = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=';
-
-	fetch(urlApi)
-		.then(function(resp) {
-			return resp.json();
-		}) // Convert data to json
-		.then(function(data) {
-			for (var i = 0; i < data.results.length; i++) {
-				for (var j = 0; j < wordList.length; j++) {
-					//search titles
-					if (
-						data.results[i]['adx_keywords'].includes(wordList[j]) ||
-						data.results[i].abstract.includes(wordList[j])
-					) {
-						console.log('This is the article: ' + data.results[i].title);
+	//no search -- get top articles
+	if (document.getElementById('search1').value == '') {
+		let urlApi = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=' + apiKey;
+		fetch(urlApi)
+			.then(function(resp) {
+				return resp.json();
+			}) // Convert data to json
+			.then(function(data) {
+				console.log('number of items: ' + data.results.length);
+				for (var i = 0; i < data.results.length; i++) {
+					var wordIncluded = false;
+					for (var j = 0; j < wordList.length; j++) {
+						//search titles
+						if (
+							data.results[i]['adx_keywords'].includes(wordList[j]) ||
+							data.results[i].abstract.includes(wordList[j]) ||
+							data.results[i].title.includes(wordList[j])
+						) {
+							wordIncluded = true;
+							console.log('This is the article: ' + data.results[i].title);
+						}
+						//console.log(wordList[j] + ' comparing with ' + data.results[i]['adx_keywords']);
 					}
-					//console.log(wordList[j] + ' comparing with ' + data.results[i]['adx_keywords']);
+					if (wordIncluded == false) {
+						var article = document.createElement('article');
+						article.className = 'story';
+
+						var img = document.createElement('img');
+						var h2 = document.createElement('h2');
+						var p = document.createElement('p');
+
+						img.src = data.results[i].media[0]['media-metadata'][2]['url'];
+						h2.innerHTML = '<a href="' + data.results[i]['url'] + '">' + data.results[i].title;
+						p.innerHTML = data.results[i].abstract;
+
+						article.appendChild(img);
+						article.appendChild(h2);
+						article.appendChild(p);
+
+						document.getElementById('topStories').appendChild(article);
+					}
 				}
-			}
-		})
-		.catch(function() {
-			// catch any errors
-		});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	} else {
+		let search1 = document.getElementById('search1').value;
+
+		let url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + search1 + '&api-key=' + apiKey;
+		fetch(url)
+			.then(function(resp) {
+				return resp.json();
+			}) // Convert data to json
+			.then(function(data) {
+				//filter and print search
+
+				for (var i = 0; i < data.response.docs.length; i++) {
+					var wordIncluded = false;
+					for (var j = 0; j < wordList.length; j++) {
+						//search titles
+						if (
+							data.response.docs[i]['lead_paragraph'].includes(wordList[j]) ||
+							data.response.docs[i]['headline'].main.includes(wordList[j])
+						) {
+							wordIncluded = true;
+							console.log('This is the article: ' + data.response.docs[i]['headline'].main);
+						}
+						//console.log(wordList[j] + ' comparing with ' + data.results[i]['adx_keywords']);
+					}
+					if (wordIncluded == false) {
+						var article = document.createElement('article');
+						article.className = 'story';
+
+						var img = document.createElement('img');
+						var h2 = document.createElement('h2');
+						var p = document.createElement('p');
+
+						img.src = 'http://www.nytimes.com/' + data.response.docs[i]['multimedia'][0]['url'];
+						h2.innerHTML =
+							'<a href="' + data.response.docs[i]['web-url'] + '">' + data.response.docs[i].headline.main;
+						p.innerHTML = data.response.docs[i].snippet;
+
+						article.appendChild(img);
+						article.appendChild(h2);
+						article.appendChild(p);
+
+						document.getElementById('topStories').appendChild(article);
+					}
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 }
 
 function customSearch() {
@@ -98,22 +179,21 @@ function customSearch() {
 				//console.log(data);
 				console.log('search success');
 				console.log('clearing page');
-				clearStories();
+				clearBox();
 
 				appendSearch(data);
 			})
-			.catch(function() {
-				// catch any errors
+			.catch((error) => {
+				console.log(error);
 			});
-	}
-	else {
+	} else {
 		var note = document.getElementById('note');
 		var content = 'No search word was added';
 		note.appendChild(content);
 	}
 }
 
-function clearStories() {
+function clearBox() {
 	var stories = document.getElementById('topStories');
 	stories.innerHTML = '';
 }
@@ -123,13 +203,6 @@ function appendSearch(data) {
 	console.log('array length: ' + data.response.docs.length);
 
 	if (data.response.docs.length == 0) {
-		/*
-		var article = document.createElement('article');
-		article.className = 'story';
-		var h2 = document.createElement('h2');
-		h2.innerHTML = 'Sorry, there are no articles from your search';
-		article.appendChild(h2);
-		*/
 		var article = document.createElement('article');
 		article.className = 'story';
 		var h2 = document.createElement('h2');
@@ -150,9 +223,10 @@ function appendSearch(data) {
 		var ul = document.createElement('ul');
 
 		img.src = 'http://www.nytimes.com/' + data.response.docs[i]['multimedia'][0].url;
+		h2.setAttribute('target', '_blank');
+		//img.target = '_blank';
 		h2.href = data.response.docs[i]['web_url'];
 		h2.innerHTML = '<h2>' + data.response.docs[i].headline.main + '</h2>';
-		//h2.innerHTML ='<a href="' + data.response.docs[i]['web_url'] + '">' + data.response.docs[i].headline.main + '>';
 		ul.innerHTML = '<li>' + data.response.docs[i].snippet + '</li>';
 
 		article.appendChild(img);
